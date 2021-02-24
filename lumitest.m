0c37d83f9ffc2da2c2b0becda608b882ea32936c
Immo Schuetz <ischtz@posteo.eu>
2021-02-24 17:16:29 +0100

    Add manual entry mode

--- lumitest.m
+++ lumitest.m
@@ -5,24 +5,31 @@ function lumitest(ser_port, screenno, pausedur, win_rect)
 % To use a different device, reimplement ph_init.m and ph_measure.m!
 %
 % Arguments:
-% ser_port      serial port device to use, e.g. 'COM4' or '/dev/ttyUSB0'
+% ser_port      serial port device to use, e.g. 'COM4' or '/dev/ttyUSB0',
+%               can be empty matrix [] to fall back to manual input
 % screenno      screen number to use for Psychtoolbox
 % pausedur      pause duration for each measurement (s)
 %
 
 % Function argument defaults
 if nargin < 1
-    ser_port = 'COM4';
-end;
+    ser_port = [];
+end
 if nargin < 2
     screenno = max(Screen('Screens'));
-end;
+end
 if nargin < 3
     pausedur = 2;
-end;
+end
 if nargin < 4
     win_rect = [];
-end;
+end
+
+manual = false;
+if isempty(ser_port)
+    manual = true;
+    fprintf('* Note: no serial port specified, falling back to manual entry!\n');
+end
 
 % Create matrix for luminance values: [R; G; B; Sum]
 lval = zeros(52 * 4, 3);
@@ -33,13 +40,17 @@ lval(157:208, 1:3) = [0:5:255; 0:5:255; 0:5:255]';
 lval(:, 4) = 0;
 
 % Set up photometer device
-phdev = ph_init(ser_port);
+if ~manual
+    phdev = ph_init(ser_port);
+end
 
 % Set up Psychtoolbox and run measurements
 try
+    KbName('UnifyKeyNames');
     %Screen('Preference', 'VBLTimestampingMode', 1); 
+    Screen('Preference','TextRenderer', 0);
     Screen('Preference', 'Verbosity', 1);    % 1 = print errors
-    Screen('Preference', 'Skipsynctests', 1);
+    Screen('Preference', 'SkipSyncTests', 1);
     
     [win, wrect] = Screen('OpenWindow', screenno, [0 0 0], win_rect);
     Screen('TextFont', win, 'Arial');
@@ -50,8 +61,12 @@ try
     WaitSecs(5);
     
     % Print baseline luminance
-    base_lum = ph_measure(phdev);
-    msg = sprintf('Baseline luminance: %2.3f cd/m^2, press ENTER to start measurements sequence!', base_lum); 
+    if ~manual
+        base_lum = ph_measure(phdev);
+        msg = sprintf('Baseline luminance: %2.3f cd/m^2, press ENTER to start measurements sequence!', base_lum); 
+    else
+        msg = sprintf('Press ENTER to start measurements sequence!');
+    end
     fprintf(msg);
     Screen('DrawText', win, msg, 10, 10, [255 255 255]);
     Screen('Flip', win);
@@ -74,7 +89,12 @@ try
         WaitSecs(pausedur);
 
         % Luminance measurement
-        lum = ph_measure(phdev);
+        if ~manual
+            lum = ph_measure(phdev);
+        else
+            response = GetEchoString(win, 'Enter value (cd/m^2): ', 10, 40, [127 127 127], color);
+            lum = str2double(response);
+        end
         lval(idx,4) = lum;
         
         fprintf('%3d\t%3d\t%3d\t%3d\t%2.3f\n', idx, color(1), color(2), color(3), lum);
@@ -99,14 +119,17 @@ try
     ylabel('Luminance (cd/m^2)');
 
     Screen('CloseAll');
-    fclose(phdev);
+    if ~manual
+        fclose(phdev);
+    end
     
 catch
-    fclose(phdev);
-    psychrethrow;
+    if ~manual
+        fclose(phdev);
+    end
     ShowCursor;
     Screen('CloseAll')
-end;
+end
 
 end
 
